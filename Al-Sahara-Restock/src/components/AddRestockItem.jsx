58db362f29f75909c3b8e24fd9f9da3a35885ad0
Jpearson6 <jpearson.dev@gmail.com>
2024-05-14 13:43:19 -0700

    Added Circular Loading

--- Al-Sahara-Restock/src/components/AddRestockItem.jsx
+++ Al-Sahara-Restock/src/components/AddRestockItem.jsx
@@ -14,10 +14,12 @@ import AspectRatio from "@mui/joy/AspectRatio";
 
 import { addNewItem, uploadImagesAndReturnURLs } from "../firebase/utils";
 import PropTypes from "prop-types";
+import CircularLoading from "./CircularLoading";
 
 export default function AddRestockItem({ open, setOpen, rows, setRows }) {
   const [images, setImages] = React.useState([]);
   const [itemName, setItemName] = React.useState("");
+  const [showLoading, setShowLoading] = React.useState(false);
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
@@ -53,6 +55,7 @@ export default function AddRestockItem({ open, setOpen, rows, setRows }) {
       return;
     }
 
+    setShowLoading(true);
     try {
       let imageUrls = [];
       if (images.length > 0) {
@@ -67,7 +70,8 @@ export default function AddRestockItem({ open, setOpen, rows, setRows }) {
           delivered: false,
           ordered: false,
         };
-
+        handleClose();
+        setShowLoading(false);
         const id = await addNewItem(newItem);
         setRows([...rows, { ...newItem, id: id }]);
       } catch (error) {
@@ -76,8 +80,11 @@ export default function AddRestockItem({ open, setOpen, rows, setRows }) {
     } catch (error) {
       console.error("Error adding item:", error);
     }
-    handleClose();
   };
+
+  if (showLoading) {
+    return <CircularLoading />;
+  }
 
   return (
     <React.Fragment>
@@ -88,90 +95,99 @@ export default function AddRestockItem({ open, setOpen, rows, setRows }) {
         fullWidth={true}
       >
         <DialogTitle>New Item</DialogTitle>
-        <DialogContent
-          sx={{
-            justifyContent: "center",
-          }}
-        >
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="item"
-            name="item"
-            label="item name"
-            type="text"
-            fullWidth
-            variant="standard"
-            value={itemName}
-            onChange={(e) => setItemName(e.target.value)}
-          />
 
-          <Stack
-            width={"100%"}
-            display={"flex"}
-            justifyContent={"center"}
-            pt={3}
-          >
-            {images && (
-              <Box
-                sx={{
-                  display: "flex",
-                  gap: 1,
-                  py: 1,
-                  overflow: "auto",
-                  width: "100%",
-                  scrollSnapType: "x mandatory",
-                  "& > *": {
-                    scrollSnapAlign: "center",
-                  },
-                  "::-webkit-scrollbar": { display: "none" },
-                }}
-              >
-                {images.map((image) => (
-                  <Card
-                    orientation="horizontal"
-                    size="sm"
-                    key={image.name}
-                    variant="outlined"
-                  >
-                    <AspectRatio
-                      key={image.name}
-                      ratio="1"
-                      sx={{ minWidth: 60 }}
-                    >
-                      <img
-                        key={image.name}
-                        src={URL.createObjectURL(image)}
-                        alt="Uploaded Image"
-                        style={{ maxWidth: "100%" }}
-                      />
-                    </AspectRatio>
-                  </Card>
-                ))}
-              </Box>
-            )}
-            <input
-              type="file"
-              accept="image/*"
-              onChange={handleFileChange}
-              capture="camera" // This attribute triggers camera capture
-              style={{ display: "none" }} // Hiding the default input appearance
-              id="image-upload"
-            />
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleCaptureClick}
+        {showLoading ? (
+          <DialogContent>
+            <CircularLoading />
+          </DialogContent>
+        ) : (
+          <>
+            <DialogContent
+              sx={{
+                justifyContent: "center",
+              }}
             >
-              Take Picture
-            </Button>
-          </Stack>
-        </DialogContent>
-        <DialogActions sx={{ justifyContent: "center" }}>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleConfirm}>Confirm</Button>
-        </DialogActions>
+              <TextField
+                autoFocus
+                required
+                margin="dense"
+                id="item"
+                name="item"
+                label="item name"
+                type="text"
+                fullWidth
+                variant="standard"
+                value={itemName}
+                onChange={(e) => setItemName(e.target.value)}
+              />
+
+              <Stack
+                width={"100%"}
+                display={"flex"}
+                justifyContent={"center"}
+                pt={3}
+              >
+                {images && (
+                  <Box
+                    sx={{
+                      display: "flex",
+                      gap: 1,
+                      py: 1,
+                      overflow: "auto",
+                      width: "100%",
+                      scrollSnapType: "x mandatory",
+                      "& > *": {
+                        scrollSnapAlign: "center",
+                      },
+                      "::-webkit-scrollbar": { display: "none" },
+                    }}
+                  >
+                    {images.map((image) => (
+                      <Card
+                        orientation="horizontal"
+                        size="sm"
+                        key={image.name}
+                        variant="outlined"
+                      >
+                        <AspectRatio
+                          key={image.name}
+                          ratio="1"
+                          sx={{ minWidth: 60 }}
+                        >
+                          <img
+                            key={image.name}
+                            src={URL.createObjectURL(image)}
+                            alt="Uploaded Image"
+                            style={{ maxWidth: "100%" }}
+                          />
+                        </AspectRatio>
+                      </Card>
+                    ))}
+                  </Box>
+                )}
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleFileChange}
+                  capture="camera" // This attribute triggers camera capture
+                  style={{ display: "none" }} // Hiding the default input appearance
+                  id="image-upload"
+                />
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleCaptureClick}
+                >
+                  Take Picture
+                </Button>
+              </Stack>
+            </DialogContent>
+            <DialogActions sx={{ justifyContent: "center" }}>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button onClick={handleConfirm}>Confirm</Button>
+            </DialogActions>
+          </>
+        )}
       </Dialog>
     </React.Fragment>
   );
@@ -181,5 +197,5 @@ AddRestockItem.propTypes = {
   rows: PropTypes.array,
   setRows: PropTypes.func,
   open: PropTypes.bool,
-  setOpen: PropTypes.func
+  setOpen: PropTypes.func,
 };
