34268caf12549be81434156ed2c77393da939d3c
Jpearson6 <jpearson.dev@gmail.com>
2024-05-14 13:44:58 -0700

    Imlemented Circular Loading

--- Al-Sahara-Restock/src/components/OrderedList.jsx
+++ Al-Sahara-Restock/src/components/OrderedList.jsx
@@ -4,14 +4,29 @@ import ImagePreview from "./ImagePreview";
 import ItemList from "./ItemList";
 import { Divider, Typography } from "@mui/material";
 import PropTypes from "prop-types";
+import CircularLoading from "./CircularLoading";
 
-export default function OrderedList({role}) {
+export default function OrderedList({ role }) {
   const [rows, setRows] = React.useState(null);
   const [selectedImage, setSelectedImage] = React.useState(null);
+  const [showLoading, setShowLoading] = React.useState(true);
 
   React.useEffect(() => {
     getOrderedItems(setRows);
   }, []);
+
+  React.useEffect(() => {
+    // Show loading for 1 second before showing the image preview
+    const timeoutId = setTimeout(() => {
+      setShowLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timeoutId);
+  }, [showLoading]);
+
+  if (showLoading) {
+    return <CircularLoading />;
+  }
 
   return (
     <>
@@ -30,11 +45,17 @@ export default function OrderedList({role}) {
           setSelectedImage={setSelectedImage}
         />
       )}
-      <ItemList rows={rows} setRows={setRows} role={role} setSelectedImage={setSelectedImage} listType={'Delivered'}/>
+      <ItemList
+        rows={rows}
+        setRows={setRows}
+        role={role}
+        setSelectedImage={setSelectedImage}
+        listType={"Delivered"}
+      />
     </>
   );
 }
 
 OrderedList.propTypes = {
-  role: PropTypes.string
+  role: PropTypes.string,
 };
